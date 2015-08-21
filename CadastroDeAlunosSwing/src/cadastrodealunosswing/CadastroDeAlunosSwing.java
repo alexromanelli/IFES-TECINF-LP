48a2a7f55f42da37463f1308a183207da75766f9
Alexandre <professor@alexromanelli.com.br>
2015-08-21 20:07:04 -0300

    Envio inicial

---
+++ CadastroDeAlunosSwing/src/cadastrodealunosswing/CadastroDeAlunosSwing.java
@@ -0,0 +1,10 @@
+package cadastrodealunosswing;
+
+public class CadastroDeAlunosSwing {
+
+    public static void main(String[] args) {
+        TelaPrincipal tela = new TelaPrincipal();
+        tela.setVisible(true);
+    }
+    
+}
